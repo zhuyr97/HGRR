a4736a3267947b1d07154b23c2e19e62ae2bb1f1
Yurui Zhu <69180896+zhuyr97@users.noreply.github.com>
2023-04-04 21:58:04 +0800

    Add files via upload

--- models/arch/__init__.py
+++ models/arch/__init__.py
@@ -2,10 +2,9 @@
 from .default import DRNet
 from .default1 import DRNet1
 from .default2 import DRNet2
-from .default3 import DRNet3,DRNet3_T
-from .default4 import DRNet4
-from .default5 import DRNet5_ori
-
+from .default3 import DRNet3
+from .default3_retrain import DRNet3_re
+from .default4 import DRNet4,DRNet4_
 import torch
 
 
@@ -16,21 +15,33 @@ def basenet(in_channels, out_channels, **kwargs):
 def errnet(in_channels, out_channels, **kwargs):
     return DRNet(in_channels, out_channels, 256, 13, norm=None, res_scale=0.1, se_reduction=8, bottom_kernel_size=1, pyramid=True, **kwargs)
 
-def train_DB_wboosting_T(in_channels, out_channels, **kwargs):
-    return DRNet3_T(in_channels, out_channels, 128, 4, norm=None, res_scale=0.1, se_reduction=8, bottom_kernel_size=1, pyramid=True,last_sigmoid=False,**kwargs)
+def errnet_whyper(in_channels, out_channels, **kwargs):
+    return DRNet1(in_channels, out_channels, 128, 4, norm=None, res_scale=0.1, se_reduction=8, bottom_kernel_size=1, pyramid=True,hyper=True,**kwargs)
 
+def errnet_wohyper(in_channels, out_channels, **kwargs):
+    return DRNet1(in_channels, out_channels, 128, 4, norm=None, res_scale=0.1, se_reduction=8, bottom_kernel_size=1, pyramid=True,hyper=False,**kwargs)
 
-def train_DB_wboosting(in_channels, out_channels, **kwargs):
+def errnet_wGlobal(in_channels, out_channels, **kwargs):
+    return DRNet2(in_channels, out_channels, 144, 4, norm=None, res_scale=0.1, se_reduction=8, bottom_kernel_size=1, pyramid=True,global_residual=True,**kwargs)
+
+def errnet_woGlobal(in_channels, out_channels, **kwargs):
+    return DRNet2(in_channels, out_channels, 144, 4, norm=None, res_scale=0.1, se_reduction=8, bottom_kernel_size=1, pyramid=True,global_residual=False,**kwargs)
+
+def errnet_wboosting(in_channels, out_channels, **kwargs):
     return DRNet3(in_channels, out_channels, 128, 4, norm=None, res_scale=0.1, se_reduction=8, bottom_kernel_size=1, pyramid=True,last_sigmoid=False,**kwargs)
 
-def train_DB_wboosting1(in_channels, out_channels, **kwargs):
-    return DRNet3(in_channels, out_channels, 96, 6, norm=None, res_scale=0.1, se_reduction=8, bottom_kernel_size=1, pyramid=True,last_sigmoid=False,**kwargs)
+def HGRR_wboosting1128(in_channels, out_channels, **kwargs):
+    return DRNet3_re(in_channels, out_channels, 128, 4, norm=None, res_scale=0.1, se_reduction=8, bottom_kernel_size=1, pyramid=True,last_sigmoid=False,**kwargs)
 
-def train_DB_wboosting2(in_channels, out_channels, **kwargs):
-    return DRNet3(in_channels, out_channels, 72, 8, norm=None, res_scale=0.1, se_reduction=8, bottom_kernel_size=1, pyramid=True,last_sigmoid=False,**kwargs)
 
-def train_DB_DRNet4_wboosting(in_channels, out_channels, **kwargs):
+def errnet_wboosting1(in_channels, out_channels, **kwargs):
+    return DRNet3(in_channels, out_channels, 144, 4, norm=None, res_scale=0.1, se_reduction=8, bottom_kernel_size=1, pyramid=True,last_sigmoid=False,**kwargs)
+
+def errnet_wboosting1_wact(in_channels, out_channels, **kwargs):
+    return DRNet3(in_channels, out_channels, 144, 4, norm=None, res_scale=0.1, se_reduction=8, bottom_kernel_size=1, pyramid=True,last_sigmoid=True,**kwargs)
+
+
+def errnet_wboosting_woR3attention(in_channels, out_channels, **kwargs):
     return DRNet4(in_channels, out_channels, 128, 4, norm=None, res_scale=0.1, se_reduction=8, bottom_kernel_size=1, pyramid=True,last_sigmoid=False,**kwargs)
-
-def train_DB_wboosting2_ori(in_channels, out_channels, **kwargs):
-    return DRNet5_ori(in_channels, out_channels, 72, 8, norm=None, res_scale=0.1, se_reduction=8, bottom_kernel_size=1, pyramid=True,last_sigmoid=False,**kwargs)
+def errnet_wboosting_woDalitedBlock(in_channels, out_channels, **kwargs):
+    return DRNet4_(in_channels, out_channels, 128, 4, norm=None, res_scale=0.1, se_reduction=8, bottom_kernel_size=1, pyramid=True,last_sigmoid=False,**kwargs)
